9f22a67d9d95ae92db11a9347d86b322656998a7
georgealv96 <george1996alvarado@gmail.com>
2023-07-17 10:39:25 -0600

    creating a NewSkillForm component and importing it to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import SkillList from './SkillList'
+import NewSkillForm from './NewSkillForm'
+import './App.css'
 
 function App() {
   return (
     <>
       <h1>React Dev Skills</h1>
       <SkillList />
+      <hr />
+      <NewSkillForm />
     </>
   )
 }
